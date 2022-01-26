a4b724642413a7621dbbb832689698b58aac6a61
savifon <savifon1@gmail.com>
2022-01-25 23:19:08 -0300

    ajustando testes com provider

--- src/pages/Login.test.js
+++ src/pages/Login.test.js
@@ -1,35 +1,50 @@
 import React from "react";
 
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 import Login from "./Login";
+import { AuthContext } from "../context/AuthContext";
+
+function renderLogin() {
+    const user = null;
+    const loading = () => {};
+    const login = () => {};
+    const logout = () => {};
+
+    return render(
+        <AuthContext.Provider
+            value={{ authenticathed: !!user, user, loading, login, logout }}
+        >
+            <Login />
+        </AuthContext.Provider>
+    );
+}
 
 describe("Página de login", () => {
     test("deve conter texto de boas vindas", () => {
-        render(<Login />);
+        renderLogin();
 
         const welcomeText = screen.getByText("Seja bem-vindo(a)");
         expect(welcomeText).toBeInTheDocument();
     });
 
     test("deve conter o input do tipo email", () => {
-        render(<Login />);
+        renderLogin();
 
         const inputEmail = screen.getByRole("textbox", { type: "email" });
         expect(inputEmail).toBeInTheDocument();
     });
 
     test("deve conter o input do tipo password", () => {
-        render(<Login />);
+        renderLogin();
 
         const inputPassword = screen.getByRole("textbox", { type: "password" });
         expect(inputPassword).toBeInTheDocument();
     });
 
     test("deve conter o botão de submit", () => {
-        render(<Login />);
+        renderLogin();
 
         const submitButton = screen.getByRole("button", { type: "submit" });
         expect(submitButton).toBeInTheDocument();
